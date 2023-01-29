c89ce8452787020d462bd31aff07eec76733cfde
miximc <miximc@mail.ru>
2023-01-29 17:41:07 +0300

    add HW

---
+++ my_script.js
@@ -0,0 +1,15 @@
+// Прислать решение "форума": на странице есть поле ввода
+// заголовка, имени и тела сообщения. Кнопка "сохранить"
+// должна приписывать к треду сообщений ваши
+
+//
+const save = () => {
+	var title = document.getElementById('text').value;
+    var user_name = document.getElementById('name').value;
+    var comment = document.getElementById('user_comments').value; 
+	document.getElementById('user_text').innerHTML = '<b>Тема:</b>'+ '<br>' + title + 
+    '<br>' + '<br>' +'<b>Имя:</b>'+'<br>'+ user_name + 
+    '<br>'+ '<br>' +'<b>Коментарий:</b>'+ '<br>' + comment;
+    
+
+}
